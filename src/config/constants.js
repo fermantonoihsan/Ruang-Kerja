5c0caa0265db45f1891bc0015d0ac66d354d4475
fermantonoihsan <fermantonoihsan@gmail.com>
2026-05-15 21:10:12 +0700

    Add constants config module

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+export const STORAGE_KEY = "atlas_workspace_state_v2";
+
+export const columns = [
+  { id: "ideas", title: "Ideas" },
+  { id: "doing", title: "Doing" },
+  { id: "review", title: "Review" },
+  { id: "done", title: "Done" },
+];
